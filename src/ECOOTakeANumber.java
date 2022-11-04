2fe3e9c51fc8a9aa19ad2d2f4241e5898d297be2
John Vinh <john.michael.vinh2@gmail.com>
2022-11-04 12:50:22 -0400

    Create BufferedReader for preparing to take input

--- src/ECOOTakeANumber.java
+++ src/ECOOTakeANumber.java
@@ -1,2 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ECOOTakeANumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        in.close();
+    }
 }
